a6f29a3e45dee7e91d2d587d3a0f0b482131ffab
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2026-07-16 12:03:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
